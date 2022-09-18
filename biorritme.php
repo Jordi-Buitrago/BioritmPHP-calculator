0b2ab5d00031f071e70f9f4b9e993a85b31fa1aa
Jordi Buitrago <jordi.buitrago@mataro.epiaedu.cat>
2022-09-18 10:27:16 +0200

    update

--- biorritme.php
+++ biorritme.php
@@ -1,3 +1,5 @@
+
+
 <?php
 $data = $_POST["data"];
 $nom = $_POST["nom"];
@@ -9,6 +11,7 @@ class Biorritme {
   private $fisic;
   private $emocional;
   private $intellectual;
+  private $porcentaje = '%';
 
 
 
@@ -22,7 +25,7 @@ class Biorritme {
   }
 
   private function formula($ciclo){
-    return round(sin(2 * pi() * ($this->diferencia / $ciclo)), 2);
+    return round(sin(2 * pi() * ($this->diferencia / $ciclo)), 2) * 100 . $this->porcentaje;
   }
 
   // Methods
@@ -59,3 +62,28 @@ $biorritme->mostrarDatos();
 $biorritme->calcularBio();
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+<body>
+  <div class="progress">
+    <div class="progress-bar bg-success" role="progressbar" aria-label="Success example" style="<?php width($fisic)?>";  aria-valuemin="0" aria-valuemax="100"></div>
+  </div>
+  <div class="progress">
+    <div class="progress-bar bg-info" role="progressbar" aria-label="Info example" style="<?php width($fisic)?>" aria-valuemin="0" aria-valuemax="100"></div>
+  </div>
+  <div class="progress">
+    <div class="progress-bar bg-warning" role="progressbar" aria-label="Warning example" style="width: " aria-valuemin="0" aria-valuemax="100"></div>
+  </div>
+  <div class="progress">
+    <div class="progress-bar bg-danger" role="progressbar" aria-label="Danger example" style="width: <?php $fisic ?>" aria-valuemin="0" aria-valuemax="100"></div>
+  </div>
+</body>
--- index.php
+++ index.php
@@ -5,17 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Biorritmes</title>
 </head>
 <body>
+    <div class="text-center mt-5">
+        <h3>Introdueix el teu nom i data de naixement per calcular el teu <h2>Biorritme</h2></h3>
+    </div>
     <form name="form" method="POST" action="biorritme.php">
-        <label for="Nom">Nom</label>
-        <input type="text" name="nom">
-        <label for="Nom">Data</label>
-        <input type="date" name="data">
-        <input type="submit" value="enviar">
-    </form>
-    
-    
+        <div class="form-row text-center justify-content-center">
+            <input type="text" name="nom" class="form-control w-50 mt-5" placeholder="Nom">
+        </div>
+        <div class="form-row text-center justify-content-center">
+            <input type="date" name="data" class="form-control w-50 mb-3">
+        </div>
+        <div class="form-group text-center">
+            <input type="submit" class="btn btn-primary" value="Enviar" class="form-control">
+        </div>    
+    </form>    
 </body>
 </html>
